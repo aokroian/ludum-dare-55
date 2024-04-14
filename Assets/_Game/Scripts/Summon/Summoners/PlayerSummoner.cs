6580ce78258045f977ed4d55546bed6e1e168d63
GodDoesPlayDice <49920714+aokroian@users.noreply.github.com>
2024-04-14 19:35:16 +0300

    switching to player input on player summoned

--- Assets/_Game/Scripts/Summon/Summoners/PlayerSummoner.cs
+++ Assets/_Game/Scripts/Summon/Summoners/PlayerSummoner.cs
@@ -1,23 +1,27 @@
 ﻿using System.Threading.Tasks;
 using _Game.Scripts.Summon.View;
+using Zenject;
 
 namespace _Game.Scripts.Summon.Summoners
 {
-    public class PlayerSummoner: Summoner
+    public class PlayerSummoner : Summoner
     {
-        
+        [Inject]
+        private GlobalInputSwitcher _globalInputSwitcher;
+
         public override async Task Summon(SummonParams summonParams)
         {
             var player = _diContainer.InstantiatePrefabForComponent<SummonedPlayer>(prefabs[0]);
-            
+
             if (_objectsHolder.rooms.Count > 0)
                 _objectsHolder.rooms[0].AddObject(player);
             else
                 _objectsHolder.objectsOutOfRoom.Add(player);
-            
+
             _objectsHolder.SetPlayerRef(player);
-            
+
             Summoned(player);
+            _globalInputSwitcher.SwitchToPlayerInput();
         }
     }
 }
