89ade326912c724a860e0d788f4fc374e43f3efc
GastarTestProjects <xe7272@gmail.com>
2024-04-15 17:41:00 +0400

    Player death ending

---
+++ Assets/_Game/Scripts/Story/GameplayEvents/PlayerDiedEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using _Game.Scripts.Message;
+using _Game.Scripts.Summon.View;
+using Zenject;
+
+namespace _Game.Scripts.Story.GameplayEvents
+{
+    public class PlayerDiedEvent: IGameplayEvent
+    {
+        public string EventId => "player_died_event";
+
+        private SummonedPlayer _player;
+        private MessageService _messageService;
+        private string _message;
+
+
+        public PlayerDiedEvent(SummonedPlayer player, MessageService messageService, string message)
+        {
+            _player = player;
+            _messageService = messageService;
+            _message = message;
+        }
+        
+        // Second ending "Shot"?
+        
+        public async Task<string> StartEvent(DiContainer diContainer)
+        {
+            _messageService.Speak(_player, _message);
+            
+            return "death";
+        }
+    }
+}
--- Assets/_Game/Scripts/Summon/View/SummonedPlayer.cs
+++ Assets/_Game/Scripts/Summon/View/SummonedPlayer.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using _Game.Scripts.CharacterRelated.Actors.ActorSystems;
 using _Game.Scripts.Story;
 using _Game.Scripts.Story.GameplayEvents;
 
@@ -6,8 +8,25 @@ namespace _Game.Scripts.Summon.View
 {
     public class SummonedPlayer : SummonedObject
     {
+        private bool _died;
+        
+        private void Start()
+        {
+            GetComponent<ActorHealth>().OnDeath += _ => OnDeath();
+        }
+        
+        private void OnDeath()
+        {
+            _died = true;
+        }
+
         public override IGameplayEvent GetEventIfAny()
         {
+            if (_died)
+            {
+                return new PlayerDiedEvent(this, MessageService, "What a miserable death...");
+            }
+            
             if (ObjectsHolder.rooms.Count == 0)
             {
                 return new PlayerInSpaceGameplayEvent(this, MessageService);
